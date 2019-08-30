e0bfab8ff656b73218559273121694ca535a0721
TayshaunMcgee <tamcg20@icstudents.org>
2019-08-30 08:57:32 -0500

    Lab 1

---
+++ abc.py
@@ -0,0 +1,46 @@
+print('sup my guy')
+
+
+name = "tayshaun mcgee"
+teach_name = name
+
+print(teach_name) 
+
+
+
+print(dir(name))
+print(name.swapcase())
+
+# Sungle Line Comment
+
+This is a multi line comment
+Print()
+
+Print(I'm not in a comment)
+
+
+a -1 
+b=2
+
+print(a+b) #addtion
+print(b+a) #subtraction
+print(b/a) #division 
+print(a/b)
+
+
+
+
+
+
+
+
+
+
+
+
+i_am_true = True
+i_am_false - False
+
+
+
+
